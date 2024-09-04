1bcef018a7e8a7a3d71d1e31cada6c5a67343f97
sazzad-anwar <sazzadzihan@gmail.com>
2024-09-04 19:23:23 +0600

    added overseer and configured neotree reveal side

--- lua/community.lua
+++ lua/community.lua
@@ -23,6 +23,31 @@ return {
     init = function() vim.g.mkdp_filetypes = { "markdown" } end,
     ft = { "markdown" },
   },
+  -- Neotree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      require("neo-tree").setup {
+        position = "right",
+        window = {
+          position = "right",
+          width = 30,
+        },
+      }
+    end,
+  },
+  -- OverSeer to run project commands
+  {
+    "stevearc/overseer.nvim",
+    opts = {},
+  },
   -- turbo console
   {
     "gaelph/logsitter.nvim",
